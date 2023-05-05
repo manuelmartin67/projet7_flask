c53a5cab173ffb866e4df4bce8b8668bcae4158f
Manuel Martin <manuelmartin67@gmail.com>
2023-05-05 10:17:12 +0200

    ajout de app.py route principale écrite route vers liste des id clients écrite

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Projet 7 - Implémentez un modèle de scoring</h1><p>Ce site donne accès à l'API pour le dashboard du " \
+           "projet 7 OC</p>"
+
+
+#app.run(port=5001)
+
+
+# une route pour renvoyer la liste des id clients
+@app.route('/api/v1/data/id_clients/all', methods=['GET'])
+def api_id_client_all():
+    SK_ID_CURR = pd.read_csv("./data/SK_ID_CURR.csv").to_dict(orient='records')
+    return jsonify(SK_ID_CURR)
+
+
+app.run(port=5001)
